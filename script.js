cc87a70c0785e2e2c32973154dd60272fe772ee0
Bahadur <bahadurosmanov@yandex.ru>
2025-09-30 00:39:09 +0200

    Числовой тип данных

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-console.log('Hello world');
-alert("Hello World");
+let firstNumber = prompt("Введите первое число", '');
+let secondNumber = prompt("Введите второе число", '');
+
+alert(parseInt(firstNumber) + parseInt(secondNumber)); //сложение
+alert(parseInt(firstNumber) - parseInt(secondNumber)); //вычитание
+alert(parseInt(firstNumber) * parseInt(secondNumber)); //умножение
+alert(parseInt(firstNumber) / parseInt(secondNumber)); //деление
